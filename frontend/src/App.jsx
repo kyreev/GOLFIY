e1f5ea4216288909af0e3e2f335d18dbc7d5ae53
dw1ki <dwikikurniawanreal@gmail.com>
2024-07-23 22:01:03 +0700

    change content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1>tolong</h1>
+    </div>
+  );
 };
 
 export default App;
