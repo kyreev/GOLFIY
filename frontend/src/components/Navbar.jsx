e4c9ea6cf68b3e88735d243af5cfe179c0028bf6
dw1ki <dwikikurniawanreal@gmail.com>
2024-07-23 22:28:18 +0700

    Add Navbar Component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div className="container">
+            <div>
+                <a 
+                    href="#"
+                    GOLFIY>
+
+                </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
